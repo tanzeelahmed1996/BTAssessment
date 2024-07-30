735bedbdef43c5ff891ffc6e60ee1ec1c4891475
Tanzeel Ahmed <57502130+tanzeelahmed1996@users.noreply.github.com>
2024-07-30 20:43:32 +0100

    Fixed unit tests

--- src/Test/LogProcessorTests.java
+++ src/Test/LogProcessorTests.java
@@ -1,9 +1,7 @@
 package Test;
 import org.junit.Before;
 import org.junit.Test;
-
 import LogFileProcessor.LogProcessor;
-
 import static org.junit.Assert.assertEquals;
 
 public class LogProcessorTests {
